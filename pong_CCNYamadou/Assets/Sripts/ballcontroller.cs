9e16e9b8aa6e557cb1d7c94a660a3008db7f61c4
lams92 <158364532+lams92@users.noreply.github.com>
2024-02-14 18:05:14 -0500

    Ball reset

--- pong_CCNYamadou/Assets/Sripts/ballcontroller.cs
+++ pong_CCNYamadou/Assets/Sripts/ballcontroller.cs
@@ -11,8 +11,26 @@ public class ballcontroller : MonoBehaviour
     public float force = 200;
     private float xDir;
     private float yDir;
+    public bool inPlay;
+    public Vector3 ballStartPos;
     // Start is called before the first frame update
     void Start()
+    {
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inPlay == false)
+        {
+            transform.position = ballStartPos;
+            Launch(); 
+        }
+   
+    }
+    
+    void Launch()
     {
         //Debug.Log("Hello world");
 
@@ -23,17 +41,41 @@ public class ballcontroller : MonoBehaviour
         if (xDir == 0)
         {
             direction.x = -1;
-        }else if (xDir == 1)
+        }
+        else if (xDir == 1)
         {
             direction.x = 1;
         }
+        
+       
+        yDir = Random.Range(0, 2);
+        //Debug.Log("yDir =" + yDir);     
+        if (yDir == 0)
+        {
+            direction.y = -1;
+        }
+        else if (yDir == 1)
+        {
+            direction.y = 1;
+     
+        }
         //add force to start movement 
         rBall.AddForce(direction * force);
+        inPlay = true;
+    }
+    
+          
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("object that collided w/ Ball:" + collision.gameObject.name);
+        if(collision.gameObject.name == "Left wall" || collision.gameObject.name == "Right wall")
+        {
+            //Debug.Log("collided with Left wall");
+            rBall.velocity = Vector3.zero;
+            inPlay = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
